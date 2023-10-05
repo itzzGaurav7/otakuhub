dccfb3fcba53acf728aee3b41d8c6da28a663015
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2023-10-05 05:35:52 +0530

    vomit

--- client/src/scenes/loginpage/index.jsx
+++ client/src/scenes/loginpage/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import {Box,Typography,useTheme,useMediaQuery} from '@mui/material'
+import Form from "./form"
 function LoginPage() {
   const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
   return (
+    <Box>
     <Box width='100%' backgroundColor={theme.palette.background.alt} p='1rem 6%'>
       <Typography
       fontWeight='bold'
@@ -11,6 +14,20 @@ function LoginPage() {
       >
         OtakuHub
       </Typography>
+    </Box>
+    <Box
+    width={isNonMobileScreens?'50%':'93%'}
+    p='2rem'
+    m='2rem auto'
+    borderRadius={theme.palette.background.alt}
+    > 
+      <Typography fontWeight='500' variant='h5' sx={{mb: "1.5rem"}}>
+            OtakuSensei is here to Welcome you! Kneel My Pupil I shall enter you now.
+      </Typography>
+      <Form/>
+
+
+    </Box>
     </Box>
   )
 }
